634a2c7532614deb8b356584a0f1b569f707c846
milandak19 <fiqrimilano@gmail.com>
2021-09-02 17:36:20 +0700

    Add and edit Task

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,8 @@ router.post('/login', Controller.Login)
 router.post('/register', Controller.register)
 router.get('/task', ControllerTask.getTask)
 router.post('/task', ControllerTask.postTask)
+router.put('/task/:id', ControllerTask.putTask)
+router.patch('/task/:id', ControllerTask.patchTask)
+router.delete('/task/:id', ControllerTask.destroyTask)
 
 module.exports = router
